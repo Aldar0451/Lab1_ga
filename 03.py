ff3629a6a8011dd2944e15fdcb8f2994e27d8573
Aldar0451 <162798861+Aldar0451@users.noreply.github.com>
2024-03-09 15:53:51 +0500

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,9 @@
+ 
+# Есть строка с перечислением фильмов
+
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+
+print(my_favorite_movies[0:10])
+print(my_favorite_movies[-15::])
+print(my_favorite_movies[12:25])
+print(my_favorite_movies[-22:-17])
---
+++ 04.py
@@ -0,0 +1,14 @@
+ 
+my_family = ["Father", "Mother", "Sister"]
+
+
+# список списков приблизителного роста членов вашей семьи
+my_family_height = [
+    # ['имя', рост],
+    ["Father", 179],
+    ["Mother", 160],
+    ["Sister", 156],
+]
+
+print("Рост отца -", my_family_height[0][1], "см")
+print("Общий рост моей семьи -", my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1], "см")
