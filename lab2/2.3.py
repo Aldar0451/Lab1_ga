ed2113cc3b828f7464bd0375c89be24e1caede3e
Aldar0451 <162798861+Aldar0451@users.noreply.github.com>
2024-04-13 15:33:35 +0500

    Update 2.3.py

--- lab2/2.3.py
+++ lab2/2.3.py
@@ -1,5 +1,7 @@
- 
+a = list()
 for m in range(2, 1000, 2):
     for n in range(1, 1001, 2):
         if 400000000<2**m * 3**n<600000000:
-            print(2**m * 3**n)
+            a.append(2**m * 3**n)
+a.sort()
+print(a)
